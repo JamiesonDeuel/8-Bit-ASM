2632b49b068c0bfa7033326f4fb53044cb4ac499
Jamieson Deuel <jkdeuel3@gmail.com>
2024-01-18 15:00:40 -0700

    Added customasm path

--- Run.bat
+++ Run.bat
@@ -3,8 +3,8 @@ SETLOCAL
 
 SET /P file=Please enter the name of the file you want to run: 
 
-SET logfiles=D:\Documents\8 Bit\%file%
+SET logfiles=D:\Documents\8-Bit-ASM\%file%
 
-D:\Downloads\customasm_win64_new\customasm.exe "%logfiles%" -f annotated,base:2,group:8 
+D:\Documents\8-Bit-ASM\customasm\customasm.exe "%logfiles%" -f annotated,base:2,group:8 
 
 C:/Users/Koby/AppData/Local/Programs/Python/Python310/python.exe "d:/Documents/8 Bit/Run.py" "%logfiles%"
