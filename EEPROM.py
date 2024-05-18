1c27222f17e41658e60165cff263e1d5942cba5e
Jamieson Deuel <jkdeuel3@gmail.com>
2024-05-17 20:13:25 -0600

    5/17

--- EEPROM.py
+++ EEPROM.py
@@ -1,19 +1,52 @@
-rom_size = 8192 
+rom_size = 2048 
 
 data = [0] * rom_size  
 
+UCODE_Save = [0b00010011,0b00010001,0b00011111,0b00010111]
+UCODE_Load = [0b01000011,0b00001111,0b00000111,0b00000111]
+# end start_reset RI Set CE 'RO 'ES 'EO
+# 7       6        5  4   3   2   1   0
 for address in range(rom_size):
     
+    setup = (address & 0b10000000000) >> 10
+    byte_sel = (address & 0b00000001000) >> 3
+    end   = (address & 0b00000000100) >> 2
+    step  = (address & 0b00000000011)
     
-    value = address & 0xff
+    if(byte_sel):
+        if(end):
+            if(step == 2):
+                UCODE_Load[1] = 0b10111101;   
+        else:
+            if(setup == 0 and step != 3):
+            
+                UCODE_Load[step] = 0b00000111
+            
+            elif (step == 3 and setup == 0):
+                UCODE_Load[3]= 0b00010111
+            else:
+                UCODE_Load = [0b01000011,0b00001111,0b00000111,0b00000111]
+        data[address] = UCODE_Load[step]
+    else:
+        if(end):
+            
+            
+            UCODE_Save[2] = 0b11010111;   
+        else:
+            if(setup == 0 and step != 3):
+            
+                UCODE_Save[step] = 0b00000111
+            
+            elif (step == 3 and setup == 0):
+                UCODE_Save[3]= 0b00010111
+            else:
+                
+                UCODE_Save = [0b00010011,0b00010001,0b00011111,0b00010111]
+        data[address] = UCODE_Save[step]
     
+   
+   
     
-    if(address & 0xF00 == 0):
-        print(bin(address))
-        data[address] = value
-
-
-
 with open("output.bin", "wb") as f:
     f.write(bytes(data))
  
